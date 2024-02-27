2d61b82a41204e3b87e8d2654d3c2188e99858b1
konrad101296 <konradslimak@o2.pl>
2024-02-27 18:49:50 +0100

    Add project files.

---
+++ tydzien 2 lekcja 4 zadanie nr.1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter company employee data");
+Console.WriteLine("Enter name: ");
+string Name = Console.ReadLine();
+Console.WriteLine("Enter surname: ");
+string Surname = Console.ReadLine();
+Console.WriteLine("Enter age: ");
+int Age = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter gender: ");
+string Gender = Console.ReadLine();
+Console.WriteLine("Enter pesel number");
+string Pesel = (Console.ReadLine());
+Console.WriteLine("enter employee number: ");
+string Number = (Console.ReadLine());
+
+Console.WriteLine("Company employee data");
+Console.WriteLine($"Name: {Name}\nSurname: {Surname}\nAge: {Age}\nGender: {Gender}\nPesel: {Pesel}\nEmployee number: {Number}");
+
