863751cbb221d842fd76952070735d63b3fddf65
konrad101296 <161070317+konrad101296@users.noreply.github.com>
2024-03-05 17:15:47 +0100

    Update Program.cs
    zamienoino w Age int na string i usunieto nizej niepotrzebne nawiasy

--- tydzien 2 lekcja 4 zadanie nr.1/Program.cs
+++ tydzien 2 lekcja 4 zadanie nr.1/Program.cs
@@ -4,13 +4,13 @@ string Name = Console.ReadLine();
 Console.WriteLine("Enter surname: ");
 string Surname = Console.ReadLine();
 Console.WriteLine("Enter age: ");
-int Age = int.Parse(Console.ReadLine());
+string Age = Console.ReadLine();
 Console.WriteLine("Enter gender: ");
 string Gender = Console.ReadLine();
 Console.WriteLine("Enter pesel number");
-string Pesel = (Console.ReadLine());
+string Pesel = Console.ReadLine();
 Console.WriteLine("enter employee number: ");
-string Number = (Console.ReadLine());
+string Number = Console.ReadLine();
 
 Console.WriteLine("Company employee data");
 Console.WriteLine($"Name: {Name}\nSurname: {Surname}\nAge: {Age}\nGender: {Gender}\nPesel: {Pesel}\nEmployee number: {Number}");
